e637e51a31bde3441043df2f874b889e0e86022a
czar-testpro <czar@testpro.io>
2025-04-17 20:06:59 +0800

    Updated LoginTests - Rename test and modified url for HW15

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -8,7 +8,7 @@ import java.time.Duration;
 
 public class LoginTests extends BaseTest {
     @Test
-    public void loginEmptyEmailPassword() {
+    public void navigationToLoginPage() {
 
 //      Added ChromeOptions argument below to fix websocket error
         ChromeOptions options = new ChromeOptions();
@@ -17,7 +17,8 @@ public class LoginTests extends BaseTest {
         WebDriver driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        String url = "https://qa.koel.app/";
+        // TODO: Fix the URL if the test fails
+        String url = "htpps://qa.koel.app/";
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
         driver.quit();
