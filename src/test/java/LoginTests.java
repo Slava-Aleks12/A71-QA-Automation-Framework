2b74b29b3e0dd575971bb1ae5d9831b2760bde0a
Slava <iaroslav.aleksandrov@testpro.io>
2025-06-07 20:33:53 -0500

    Update LoginTests.java

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -18,7 +18,7 @@ public class LoginTests extends BaseTest {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
         // TODO: Fix the URL if the test fails
-        String url = "https://testpro.io/";
+        String url = "https://qa.koel.app/";
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
         driver.quit();
