57ce634455e6dbe1ade406a92eca12bde1905cf3
Slava-Aleks12 <iaroslav.aleksandrov@testpro.io>
2025-06-10 19:36:35 -0500

    updated the code as it should look

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -8,26 +8,21 @@ import org.testng.annotations.Test;
 
 import java.time.Duration;
 
+@Test
 public class LoginTests extends BaseTest {
 
-
     @Test
-    public void LoginValidEmail() throws InterruptedException {
+    public void registrationNavigation() throws InterruptedException {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
 
         WebDriver driver = new ChromeDriver(options);
-        String Url= "https://qa.koel.app/";
+        String Url = "https://qa.koel.app/";
         driver.get(Url);
-        WebElement emailFiled= driver.findElement(By.cssSelector("a[href='registration']"));
-        //emailFiled.sendKeys("iaroslav.aleksandrov@testpro.io");
-        //WebElement passwordField= driver.findElement(By.cssSelector("input[type='password']"));
-        //passwordField.sendKeys("iTedHBsr");
-        //WebElement submitButton= driver.findElement(By.cssSelector("button[type='submit']"));
-        //submitButton.click();
-        Thread.sleep(2000);
-        //WebElement avatar= driver.findElement(By.cssSelector("img[class='avatar']"));
-        //Assert.assertTrue(avatar.isDisplayed());
+        WebElement registrationNavigation = driver.findElement(By.cssSelector("a[href='registration']"));
+        registrationNavigation.click();
+        Thread.sleep(5000);
         driver.quit();
     }
+
 }
